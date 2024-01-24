16b446cfed9b920862d72566bcfda5da9c1a88de
Pedro Julian Soto Badillo <pedrojuliaann@gmail.com>
2024-01-24 13:53:52 -0600

    Se realizaron cambios como agregar componentes, servicios, guardianes version 1.1

---
+++ src/app/primeng.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
+
+const modPrimeNg: any = [
+  ButtonModule
+] ;
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    modPrimeNg,
+    ButtonModule
+  ], exports: [
+    modPrimeNg
+  ]
+})
+export class PrimengModule { }
