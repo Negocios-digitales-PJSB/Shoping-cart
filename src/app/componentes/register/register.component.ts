71fa1b39925e895d1081d79a72e4bed4d381b327
Pedro Julian Soto Badillo <pedrojuliaann@gmail.com>
2024-02-02 10:24:47 -0600

    Version de modificacion del register sin validacion 5

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,20 +1,53 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
+
+
 export class RegisterComponent {
-registerForma = this.fb.group({
-  "name": ['',[Validators.required]]
-});
 
+  ngOnInit() {
+    this.registerForma.valueChanges.subscribe(value => console.log(value));
+  }
 
-constructor(private fb:FormBuilder){
+  registerForma = this.fb.group({
+    fullName: ['', [Validators.required, Validators.pattern(/(?:[a-zA-Z]+)*$/), Validators.minLength(8)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.[a-z])(?=.[A-Z])(?=.[!@#$%^&])/)]], // (/^(?=.^[a-z])(?=.^[A-Z])[0-9a-zA-Z]{8,}$/)] //^(?=[a-z])(?=[A-Z])[0-9a-zA-Z]{8,}$
 
-}
+    confirmPassword: ['', [Validators.required]],
+  }, { validator: this.passwordMatchValidator });
 
+  constructor(private fb: FormBuilder) {}
 
+  get fullName() {
+    return this.registerForma.controls['fullName']
+  }
+
+  get email() {
+    return this.registerForma.controls['email'];
+  }
+
+  get password() {
+    return this.registerForma.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForma.controls['confirmPassword'];
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+  
+    if (password && confirmPassword) {
+      return password === confirmPassword ? null : { passwordMismatch: true };
+    }
+  
+    return null; // Si uno de los campos está vacío, no aplicar la validación
+  }
 }
